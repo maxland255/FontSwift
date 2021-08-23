4245c21804ebc377ee62c370b81c36d9549dec71
Maxland255 <maxland255@gmail.com>
2021-08-23 18:19:05 +0200

    V0.4 beta 4

--- Sources/FontPackage/VerticalPickerColor.swift
+++ Sources/FontPackage/VerticalPickerColor.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
-public struct ColorPicker: View {
+struct ColorPicker: View {
     
-    @State var black = false
+    @State private var black = false
     
     @Binding var chosenColor: Color
     @State private var isDragging: Bool = false
     @State private var startLocation: CGFloat = .zero
     @State private var dragOffset: CGSize = .zero
         
+    private var color: [Color] = [Color.black, Color.white]
+    
     private var colors: [Color] = {
         let hueValues = Array(0...359)
         return hueValues.map {
@@ -28,9 +30,9 @@ public struct ColorPicker: View {
     }
         
     private var linearGradientHeight: CGFloat = 200
-        
+    
     private var currentColor: Color {
-        Color(UIColor.init(hue: 1.0, saturation: 0, brightness: self.normalizeGesture() / linearGradientHeight, alpha: 1.0))
+        !black ? Color(UIColor.init(hue: self.normalizeGesture() / linearGradientHeight, saturation: 1.0, brightness: 1.0, alpha: 1.0)) : Color(UIColor.init(hue: 1.0, saturation: 0, brightness: self.normalizeGesture() / linearGradientHeight, alpha: 1.0))
     }
         
     private func normalizeGesture() -> CGFloat {
@@ -42,43 +44,56 @@ public struct ColorPicker: View {
     }
         
     public var body: some View {
-        ZStack(alignment: .top) {
-            LinearGradient(gradient: Gradient(colors: [Color.black, Color.white]), startPoint: .top, endPoint: .bottom)
-                .frame(width: 10, height: linearGradientHeight)
-                .cornerRadius(5)
-                .shadow(radius: 8)
-                .overlay(
-                        RoundedRectangle(cornerRadius: 5).stroke(Color.white, lineWidth: 2.0)
-                )
-                .gesture(
-                    DragGesture()
-                        .onChanged({ (value) in
-                            self.dragOffset = value.translation
-                            self.startLocation = value.startLocation.y
-                            self.chosenColor = self.currentColor
-                            self.isDragging = true
-                        })
-                        .onEnded({ (_) in
-                            self.isDragging = false
-                        })
-                )
-                
-            Circle()
-                .foregroundColor(self.currentColor)
-                .frame(width: self.circleWidth, height: self.circleWidth, alignment: .center)
-                .shadow(radius: 5)
-                .overlay(
-                    RoundedRectangle(cornerRadius: self.circleWidth / 2.0).stroke(Color.white, lineWidth: 2.0)
-                )
-                .offset(x: self.isDragging ? -self.circleWidth : 0.0, y: self.normalizeGesture() - self.circleWidth / 2)
-                .animation(Animation.spring().speed(2))
+        VStack{
+            ZStack(alignment: .top) {
+                LinearGradient(gradient: Gradient(colors: black ? color : colors), startPoint: .top, endPoint: .bottom)
+                    .frame(width: 10, height: linearGradientHeight)
+                    .cornerRadius(5)
+                    .shadow(radius: 8)
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 5).stroke(Color.white, lineWidth: 2.0)
+                    )
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ (value) in
+                                self.dragOffset = value.translation
+                                self.startLocation = value.startLocation.y
+                                self.chosenColor = self.currentColor
+                                self.isDragging = true
+                            })
+                            .onEnded({ (_) in
+                                self.isDragging = false
+                            })
+                    )
+                    
+                Circle()
+                    .hoverEffect(.automatic)
+                    .foregroundColor(self.currentColor)
+                    .frame(width: self.circleWidth, height: self.circleWidth, alignment: .center)
+                    .shadow(radius: 5)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: self.circleWidth / 2.0).stroke(Color.white, lineWidth: 2.0)
+                    )
+                    .offset(x: self.isDragging ? -self.circleWidth : 0.0, y: self.normalizeGesture() - self.circleWidth / 2)
+                    .animation(Animation.spring().speed(2))
+            }
+            
+            Button(action: {
+                self.black.toggle()
+            }) {
+                Circle()
+                    .fill(
+                        AngularGradient(gradient: Gradient(colors: black ? colors : color), center: .center)
+                    )
+                    .frame(width: 35, height: 35)
+            }.padding(.top, 20)
         }
     }
 }
 
 extension ColorPicker{
-    public init(chosenColor: Binding<Color>) {
-        self._chosenColor = chosenColor
+    public init(chosencolor: Binding<Color>) {
+        self._chosenColor = chosencolor
     }
 }
 
